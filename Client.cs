d412de3ce02abae9d54f19fa8983a846e3027151
Luis Choloquinga <luis.choloquinga@outlook.com>
2021-01-21 13:07:13 -0500

    project socket

---
+++ Client.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using Serialization;
+
+namespace Client
+{
+    public class Client
+    {
+        IPHostEntry host;
+        IPAddress ipAdress;
+        IPEndPoint endPoint;
+
+        Socket s_Client;
+
+        public Client(string ip,int port)
+        {
+            host = Dns.GetHostEntry(ip);
+            ipAdress = host.AddressList[0];
+            endPoint = new IPEndPoint(ipAdress, port);
+            //Init Client
+            s_Client = new Socket(ipAdress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void Start()
+        {
+            s_Client.Connect(endPoint);
+        }
+        public void Send(string msg)
+        {
+            byte[] byteMsg = Encoding.ASCII.GetBytes(msg);
+            s_Client.Send(byteMsg);
+            Console.WriteLine("Message Sended.");
+        }
+        public string Received()
+        {
+            byte[] buffer = new byte[1024];
+            s_Client.Receive(buffer);
+            return byte2String(buffer);
+        }
+        public void SendObject(object toSend)
+        {
+            s_Client.Send(BinarySerialization.Serializate(toSend));
+        }
+        public string byte2String(byte[] buffer)
+        {
+            string message;
+            int endIndex;
+            message = Encoding.ASCII.GetString(buffer);
+            endIndex = message.IndexOf('\0');
+            if (endIndex > 0)
+                message = message.Substring(0, endIndex);
+            return message;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Auth;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client("127.0.0.1",5050);
+            client.Start();
+            User u;
+            string user;
+            string password;
+            while (true)
+            {
+                Console.WriteLine("Add User:");
+                user = Console.ReadLine();
+                Console.WriteLine("Add Password:");
+                password = Console.ReadLine();
+                u = new User(user,password);
+                client.SendObject(u);
+                //Received from server
+                Console.WriteLine("Message from Server: " + client.Received());
+            }
+        }
+    }
+}
